726cf319f56e161e0ff256a1fa5f627230b76b14
griffinjoshs <leveloutusa@gmail.com>
2025-02-05 00:58:09 -0500

    rrd project

--- src/themes/HighTechNeon/Sections/Projects/ProjectItems/ProjectItem2.js
+++ src/themes/HighTechNeon/Sections/Projects/ProjectItems/ProjectItem2.js
@@ -1,6 +1,7 @@
 // projectItem2.js
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom"; // Import Link from react-router-dom
 import "./project-item2.css";
 import Button from "../../../Components/Buttons/Button";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -11,33 +12,42 @@ import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 const ProjectItem = ({ project, index }) => {
   const animationClass = index % 2 === 0 ? "left" : "right";
   console.log(project);
+  
+  // Determine if the slug is internal (starts with a slash)
+  const isInternal = project.slug.startsWith("/");
+
+  // Prepare the content that will be wrapped in either a Link or an <a>
+  const content = (
+    <>
+      <div className="project-content flex column text-left">
+        <h5>{project.name}</h5>
+        <p className="project-item-onhover p-small smaller-top-space">
+          {project.description}
+        </p>
+      </div>
+      <div className="project-item-icon project-item-onhover">
+        <FontAwesomeIcon className="arrow" size="xl" icon={faArrowRight} />
+      </div>
+    </>
+  );
+
   return (
     <div className="project-item flex column justify-center responsive-center">
       {/* Use a plain div with fade-in-<left/right> */}
       <div className={`fade-in-${animationClass}`}>
-        <a
-          href={project.slug}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="project-link"
-          style={{
-            backgroundImage: `url(${project.image || "https://picsum.photos/600/400"})`,
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-            height: "350px",
-          }}
-        >
-          <div className="project-content flex column text-left">
-            <h5>{project.name}</h5>
-            <p className="project-item-onhover p-small smaller-top-space">
-              {project.description}
-            </p>
-          </div>
-          <div className="project-item-icon project-item-onhover">
-            <FontAwesomeIcon className="arrow" size="xl" icon={faArrowRight} />
-          </div>
-        </a>
-      </div>
+          <Link
+            to={project.slug}
+            className="project-link"
+            style={{
+              backgroundImage: `url(${project.image || "https://picsum.photos/600/400"})`,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+              height: "350px",
+            }}
+          >
+            {content}
+          </Link>
+             </div>
     </div>
   );
 };
@@ -47,7 +57,7 @@ ProjectItem.propTypes = {
     id: PropTypes.number.isRequired,
     name: PropTypes.string.isRequired,
     description: PropTypes.string.isRequired,
-    link: PropTypes.string.isRequired,
+    slug: PropTypes.string.isRequired, // Now using slug for routing
     image: PropTypes.string.isRequired,
   }).isRequired,
   index: PropTypes.number.isRequired,
